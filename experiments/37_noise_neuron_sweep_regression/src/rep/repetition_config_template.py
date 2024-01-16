9fadc18149bca9549a75f451701801b465307029
dajojo <dajojo1403@gmail.com>
2024-01-16 11:21:59 +0100

    added neuron sweep experiment

---
+++ experiments/37_noise_neuron_sweep_regression/src/rep/repetition_config_template.py
@@ -0,0 +1,38 @@
+import numpy as np
+import exputils as eu
+import function_regression
+
+config = eu.AttrDict(
+    # random seed for the repetition
+    seed = 1234 + <repetition_id>,
+
+
+    model = eu.AttrDict(
+        cls=function_regression.models.<model>,
+        in_features=<in_features>,
+        out_features=<out_features>,
+        use_rbf=<use_rbf>,
+        univariate=<univariate>,
+        difficulty=<difficulty>,
+        hidden_features=<hidden_features>,
+        range=<model_range>,
+        scale=<scale>
+        ),
+
+    function = eu.AttrDict(
+        cls=function_regression.functions.<function>,
+        difficulty=<difficulty>,
+        in_features=<in_features>,
+        ranges=<func_ranges>,
+        peak_distr_ranges=<peak_distr_ranges>,
+        sample_rates = [20,20]
+        ),
+    
+    trainer = eu.AttrDict(
+        cls=function_regression.<trainer>,
+        learning_rate=<learning_rate>,
+        rbf_learning_rate=<rbf_learning_rate>,
+        n_epochs=<n_epochs>,
+        batch_size=<batch_size>),
+
+)
